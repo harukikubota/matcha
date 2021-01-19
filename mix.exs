157a35d87879306f6a4176d7542c8497163eb477
HarukiKubota <ricca545@icloud.com>
2021-01-19 22:09:02 +0900

    update: mix.exs

--- mix.exs
+++ mix.exs
@@ -7,14 +7,15 @@ defmodule Matcha.MixProject do
       version: "0.1.0",
       elixir: "~> 1.11.2",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      preferred_cli_env: [espec: :test],
     ]
   end
 
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger]
+      extra_applications: [:logger, :parsetools, :iex]
     ]
   end
 
